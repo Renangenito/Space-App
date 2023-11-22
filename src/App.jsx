dd3a289301becf52488fa1fc8de3b39d778cd561
renan lima <renangenito@hotmail.com>
2023-11-22 17:49:59 -0300

    Subindo o andamento do projeto já com barra lateral e algumas imagens

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { styled } from "styled-components";
+import EstilosGlobais from "./componentes/EstilosGrobais";
+import Cabecalho from "./componentes/Cabecalho";
+import BarraLateral from "./componentes/BarraLateral";
+import Banner from "./componentes/Banner";
+import backgroundImage from "./assets/banner.png";
+import Galeria from "./componentes/Galeria";
+import fotos from './fotos.json'
+import { useState } from "react";
+
+const FundoGradiente = styled.div`
+  background: var(
+    --Gradiente-fundo,
+    linear-gradient(175deg, #041833 4.16%, #04244f 48%, #154580 96.76%)
+  );
+  width: 100%;
+  min-height: 100vh;
+`;
+
+const AppContainer = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  max-width: 100%;
+`;
+
+const MainContainer = styled.main`
+  display: flex;
+  gap: 24px;
+`;
+
+const ConteudoGakeria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`;
+
+const App = () => {
+  const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos);
+  return (
+    <>
+      <FundoGradiente>
+        <EstilosGlobais />
+        <AppContainer>
+          <Cabecalho />
+          <MainContainer>
+            <BarraLateral />
+            <ConteudoGakeria>
+              <Banner
+                backgroundImage={backgroundImage}
+                texto="A galeria mais completa de fotos do espaço!"
+              />
+              <Galeria fotos={fotosDaGaleria}/>
+            </ConteudoGakeria>
+          </MainContainer>
+        </AppContainer>
+      </FundoGradiente>
+    </>
+  );
+}
+
+export default App;
